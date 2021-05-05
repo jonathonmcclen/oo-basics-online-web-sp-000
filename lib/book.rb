152197c69949c617e76b96afb649133167805688
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-05 20:04:00 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,11 @@
 class Book
  
-  def initialize(title)
-    @title = title
-  end
+  
   
   attr_accessor :author, :page_count, :genre
- 
+ def initialize(title)
+    @title = title
+  end
   def title
     @title
   end
